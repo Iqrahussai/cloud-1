aecc2b9e20d2895920924f414fbc5f5f5541c2ef
Ayesha Javed <ayesha18javed.com>
2026-02-25 15:45:02 +0500

    first

---
+++ condition statment.py
@@ -0,0 +1,8 @@
+marks = float(input("enter your marks:"))
+
+if marks > 60:
+    print("congratulations you're passed", marks)
+    percetange = marks/100*100
+    print("your percentage is:",percetange, "%" )
+
+print("program end")

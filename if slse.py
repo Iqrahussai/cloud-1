7ea357046b37fd5710b859de67b5e9f226b9071f
Ayesha Javed <ayesha18javed.com>
2026-02-28 15:54:35 +0500

    if else

---
+++ if slse.py
@@ -0,0 +1,23 @@
+#age = float(input("enter your age "))
+
+#if age >= 10:
+ #   print("welcome you're able to proceed your ID card")
+
+
+  #  marks = float(input("enter your marks"))
+    
+    if marks >=60:
+        print("congratulations you're passed")
+ #else:
+ #   print("sorry you're failed, try again")
+
+
+#check whether the number is positive or negative
+
+number = int(input("please enter a numberof your choice>>"))
+
+
+if number >0:
+    print("The Number", number,"is positive")
+else:
+    print("The Number", number ,"is Neegative")    
